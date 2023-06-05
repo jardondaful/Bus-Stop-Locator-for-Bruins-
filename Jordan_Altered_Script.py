e88fbc725f02407a2594ef6106740a2b18c8a2c1
Jordan Jonathan Lin <68207907+jardondaful@users.noreply.github.com>
2023-06-04 19:36:36 -0700

    Update Jordan_Altered_Script.py

--- Jordan_Altered_Script.py
+++ Jordan_Altered_Script.py
@@ -40,6 +40,20 @@ def convert_gtfs_txt_to_tables(folder_path, gtfs_folders):
         arcpy.conversion.TableToTable(gtfs_trips, export_gdb, trips_table_name)
 
         print(f"GTFS text files converted to editable tables for {gtfs_folder}!")
+
+    # Set the workspace to the geodatabase
+    arcpy.env.workspace = export_gdb
+
+    # Alter field types to text for all fields in the tables
+    arcpy.management.AlterFields(stop_times_table, "agency_id TEXT; stop_id TEXT; trip_id TEXT; arrival_time TEXT; departure_time TEXT; stop_sequence TEXT; pickup_type TEXT; drop_off_type TEXT; shape_dist_traveled TEXT")
+    arcpy.management.AlterFields(stops_table, "stop_id TEXT; stop_code TEXT; stop_name TEXT; stop_desc TEXT; stop_lat TEXT; stop_lon TEXT; zone_id TEXT; stop_url TEXT; location_type TEXT; parent_station TEXT; wheelchair_boarding TEXT")
+    arcpy.management.AlterFields(trips_table, "route_id TEXT; service_id TEXT; trip_id TEXT; trip_headsign TEXT; direction_id TEXT; block_id TEXT; shape_id TEXT")
+
+    print("Field types converted to text for all tables.")
+
+    # Return the paths to the exported tables
+    return stop_times_table, stops_table, trips_table
+
         
 def create_joins(folder_path, gtfs_folders):
     print("CREATING JOINS")
@@ -60,12 +74,15 @@ def create_joins(folder_path, gtfs_folders):
         trips_table = os.path.join(gtfs_path, "TripsExport")
 
         # Join stops to stop_times via stop_id
-        arcpy.management.JoinField(stop_times_table, "stop_id", stops_table, "stop_id", stops_table_name)
+        # Specify the field mapping with explicit field types as text
+        arcpy.management.JoinField(stop_times_table, "stop_id", stops_table, "stop_id", stops_table_name, "agency_id TEXT;stop_name TEXT;stop_lat TEXT;stop_lon TEXT")
 
         # Join trips to stop_times via trip_id
-        arcpy.management.JoinField(stop_times_table, "trip_id", trips_table, "trip_id", trips_table_name)
+        # Specify the field mapping with explicit field types as text
+        arcpy.management.JoinField(stop_times_table, "trip_id", trips_table, "trip_id", trips_table_name, "route_id TEXT")
 
         print(f"Joins created for {gtfs_folder}!")
+
 
 
 def add_agency_id_field(folder_path, gtfs_folders, gtfs_joined_folders):
@@ -125,7 +142,7 @@ def get_user_location():
 
     return user_latitude, user_longitude
 
-def find_closest_stop(user_latitude, user_longitude, merged_stop_times_table, folder_path):
+def find_closest_source_stop(user_latitude, user_longitude, merged_stop_times_table, folder_path):
     # Set the workspace
     arcpy.env.workspace = folder_path
     arcpy.env.overwriteOutput = True
@@ -149,6 +166,39 @@ def find_closest_stop(user_latitude, user_longitude, merged_stop_times_table, fo
 
     return closest_stop_id, closest_distance
 
+def find_closest_valid_destination_stop(source_stop_id, merged_stop_times_table, stops_table):
+    # Set the workspace
+    arcpy.env.workspace = FolderPath
+    arcpy.env.overwriteOutput = True
+
+    # Get the route_id and agency_id of the source stop
+    with arcpy.da.SearchCursor(stops_table, ["route_id", "agency_id"], f"stop_id = '{source_stop_id}'") as cursor:
+        for row in cursor:
+            route_id = row[0]
+            source_agency_id = row[1]
+
+    # Create a search cursor to iterate over the merged stop_times table
+    fields = ["stop_id", "stop_lat", "stop_lon"]
+    with arcpy.da.SearchCursor(merged_stop_times_table, fields) as cursor:
+        closest_stop_id = None
+        closest_distance = float("inf")
+
+        for row in cursor:
+            stop_id, stop_lat, stop_lon = row
+
+            # Check if the stop has the same route_id and agency_id as the source stop
+            with arcpy.da.SearchCursor(stops_table, ["route_id", "agency_id"], f"stop_id = '{stop_id}'") as route_cursor:
+                for route_row in route_cursor:
+                    if route_row[0] == route_id and route_row[1] == source_agency_id:
+                        # Calculate the distance between the source stop and the current stop
+                        distance = calculate_distance(source_stop_lat, source_stop_lon, stop_lat, stop_lon)
+
+                        # Update the closest stop if a closer one with the same route_id and agency_id is found
+                        if distance < closest_distance:
+                            closest_stop_id = stop_id
+                            closest_distance = distance
+
+    return closest_stop_id, closest_distance
 
 
 def main():
@@ -163,17 +213,18 @@ def main():
         "BigBlue_GTFS_StopTimes"
     ]
 
-    convert_gtfs_txt_to_tables(FolderPath, GTFSFolders)
-    print("\n")
-    create_joins(FolderPath, GTFSFolders)
-    print("\n")
-    add_agency_id_field(FolderPath, GTFSFolders, GTFSJoinedFolders)
-    print("\n")
-    merge_stop_times_tables(FolderPath, GTFSFolders, "MergedTable")
-    print("\n")
+#     convert_gtfs_txt_to_tables(FolderPath, GTFSFolders)
+#     print("\n")
+#     create_joins(FolderPath, GTFSFolders)
+#     print("\n")
+#     add_agency_id_field(FolderPath, GTFSFolders, GTFSJoinedFolders)
+#     print("\n")
+#     merge_stop_times_tables(FolderPath, GTFSFolders, "MergedTable")
+#     print("\n")
 
     user_latitude, user_longitude = get_user_location()
-    closest_stop_id, closest_distance = find_closest_stop(
+    print("Finding closest bus stop...")
+    closest_stop_id, closest_distance = find_closest_source_stop(
         user_latitude,
         user_longitude,
         os.path.join(FolderPath, "MergedTable"),
@@ -183,5 +234,9 @@ def main():
     print("\n")
     print(f"The closest bus stop is {closest_stop_id} which is {closest_distance} units away from your location.")
     print("\n")
+    
+    # Make sure it has same route ID
+    # Either only search within same bus system
+    # Or go through ALL of them to see if the closest one also has same route ID
 
 main()
