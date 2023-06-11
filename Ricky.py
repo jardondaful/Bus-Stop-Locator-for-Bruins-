21265cc4a28ae01a38e0fa9f5b4ff32b5360813b
Jordan Jonathan Lin <68207907+jardondaful@users.noreply.github.com>
2023-06-10 19:37:12 -0700

    Update Ricky.py

--- Ricky.py
+++ Ricky.py
@@ -1,7 +1,7 @@
 import arcpy
 import requests
 
-def geocode_address(address):
+def geocode_address(address, label):
     # Set up geocoding service URL
     url = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates"
 
@@ -29,26 +29,37 @@ def geocode_address(address):
             latitude = location["y"]
             longitude = location["x"]
 
-            print("Latitude: {}, Longitude: {}".format(latitude, longitude))
+            print("{} - Latitude: {}, Longitude: {}".format(label, latitude, longitude))
 
-            # Create a new shapefile
-            output_folder = r"C:\Users\rmiramo214\Desktop\181C Final Project\MyProject26\MyProject26"
-            output_shapefile = "Geocoded_point.shp"
-            arcpy.env.workspace = output_folder
-            arcpy.management.CreateFeatureclass(output_folder, output_shapefile, "POINT", spatial_reference=arcpy.SpatialReference(4326))
-
-            # Create a new feature and insert the point
-            with arcpy.da.InsertCursor(output_shapefile, ['SHAPE@XY']) as cursor:
-                cursor.insertRow([(longitude, latitude)])
-
-            print("Shapefile created successfully.")
+            return (longitude, latitude)  # Return the coordinates
         else:
             print("No candidates found.")
     else:
         print("Request failed.")
 
-    print("Done geocoding")
+    return None  # Return None if geocoding fails
+
 
 # Example usage
-address = "581 Gayle Ave, Los Angeles, CA"
-geocode_address(address)
+starting_address = "100 Hollywood Blvd, Los Angeles, CA"
+desired_address = "200 S Grand Ave, Los Angeles, CA"
+
+starting_coords = geocode_address(starting_address, "Starting Location")
+desired_coords = geocode_address(desired_address, "Desired Location")
+
+if starting_coords and desired_coords:
+    # Create a new shapefilea
+    output_folder = r"C:\Users\Jordan Lin\Downloads\GEOG_181C\MyProject26\MyProject26"
+    output_shapefile = "geocoded_points.shp"
+    arcpy.env.workspace = output_folder
+    arcpy.management.CreateFeatureclass(output_folder, output_shapefile, "POINT", spatial_reference=arcpy.SpatialReference(4326))
+
+    # Add a field to store the location labels
+    arcpy.management.AddField(output_shapefile, "Location", "TEXT")
+
+    # Create a new feature and insert the points
+    with arcpy.da.InsertCursor(output_shapefile, ['SHAPE@XY', 'Location']) as cursor:
+        cursor.insertRow([(starting_coords[0], starting_coords[1]), "Starting Location"])
+        cursor.insertRow([(desired_coords[0], desired_coords[1]), "Desired Location"])
+
+    print("Shapefile created successfully.")
